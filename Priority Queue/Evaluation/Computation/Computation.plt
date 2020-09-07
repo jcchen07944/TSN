3d5dc95d19ed76962f9713b747a6bb9ff719ccb2
JCChen <james35022000@gmail.com>
2020-09-08 04:26:07 +0800

    Computation time.

---
+++ Priority Queue/Evaluation/Computation/Computation.plt
@@ -0,0 +1,15 @@
+set xlabel "Number of deployed streams" font "Arial,12"
+set ylabel "Time (Second)" font "Arial,12"
+
+set xtics
+set ytics
+set key left top Left reverse nobox
+set terminal png font "Arial,12"
+set output "Computation.png"
+set xtics 0 ,20 ,200
+
+set title "Computation Time" font "Arial,16"
+# Plotting
+plot \
+"Computation_ATS.dat" using 1:2 with lines linewidth 2 title "ATS", \
+"Computation_TDMA.dat" using 1:2 with lines linewidth 2 title "TDMA-based" \
--- Priority Queue/StreamReserve.cpp
+++ Priority Queue/StreamReserve.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <vector>
+#include <ctime>
 
 #include "Switch.h"
 #include "EndDevice.h"
@@ -14,7 +15,7 @@ int main() {
     Utility utility;
 
     const int END_DEVICE_COUNT = 2;
-    const int SWITCH_COUNT = 2;
+    const int SWITCH_COUNT = 1;
 
     std::vector<EndDevice*> ed;
     std::vector<Switch*> sw;
@@ -26,13 +27,12 @@ int main() {
         sw.push_back(new Switch(i + END_DEVICE_COUNT));
 
     utility.connectToSwitch(sw[0], ed[0]);
-    utility.connectToSwitch(sw[1], ed[1]);
-    utility.connectToSwitch(sw[0], sw[1]);
+    utility.connectToSwitch(sw[0], ed[1]);
     //utility.connectToSwitch(sw[1], sw[2]);
 
     utility.broadcastEndDevice(sw, ed);
 
-    int TSN_FLOW_COUNT = 123;
+    int TSN_FLOW_COUNT = 200;
     int AVB_FLOW_COUNT = 0;
     int BE_FLOW_COUNT = 0;
     std::vector<Flow*> TSN;
@@ -51,16 +51,27 @@ int main() {
     //utility.setupAVB(AVB[0], 'A', 400, 0, 1, 0); // 2.56%
     //utility.setupAVB(AVB[1], 'A', 800, 0, 1, 0); // 5.12%
 
+    FILE *pFile;
+    pFile = fopen("Computation.txt", "w");
+
     for(int i = 0; i < TSN_FLOW_COUNT; i++) {
         utility.setupTSN(TSN[i], 150, 64, 0, 1, 0);
         TSN[i]->hop_count = 1;
         TSN[i]->priority = 7;
+        // Timer
+        std::clock_t timer_start = std::clock();
+
         utility.reserveTSN(TSN[i], sw, ed);
+
+        double duration = (std::clock() - timer_start) / (double) CLOCKS_PER_SEC;
+        fprintf (pFile, "%d %f\n", i+1, duration);
     }
+
+    fclose (pFile);
 
     utility.resetNetworkTime(sw, ed);
     long long time = 0;
-    while(time < 1000000) { // 1 second
+    while(time < 100000) { // 1 second
         for(int i = 0; i < TSN_FLOW_COUNT; i++)
             TSN[i]->run(ed[TSN[i]->source], TSN_FLOW);
         for(int i = 0; i < AVB_FLOW_COUNT; i++)
